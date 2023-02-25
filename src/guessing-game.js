c04b62abff72cc4173278cb5c85377648324d683
Sobyt <kravczov.andrian@mail.ru>
2023-02-25 14:52:21 +0300

    done

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -2,19 +2,21 @@ class GuessingGame {
     constructor() {}
 
     setRange(min, max) {
-
+        this.min = min;
+        this.max = max;
     }
 
     guess() {
-
+        this.guessNumber = Math.round((this.min+this.max)/2)
+        return this.guessNumber
     }
 
     lower() {
-
+        this.max = this.guessNumber
     }
 
     greater() {
-
+        this.min = this.guessNumber
     }
 }
 
